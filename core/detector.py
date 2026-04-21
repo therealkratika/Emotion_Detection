f57e8035c020f9b58fb6c75411a2a546ceaa5a0f
Kratika Gupta <therealkratikagupta@gmail.com>
2026-04-22 00:41:05 +0530

    fixed a bug

--- core/detector.py
+++ core/detector.py
@@ -2,22 +2,21 @@ import cv2
 import numpy as np
 from deepface import DeepFace
 
-# ================= FACE DETECTOR =================
 face_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 )
 
-# ================= GLOBAL =================
-last_emotion = "Neutral"
+last_emotion = "neutral"
 last_confidence = 0
 frame_count = 0
 
+emotion_history = []
+
 def process_frame(frame):
-    global last_emotion, last_confidence, frame_count
+    global last_emotion, last_confidence, frame_count, emotion_history
 
     frame_count += 1
 
-    # 🔥 Resize for speed
     frame_small = cv2.resize(frame, (320, 240))
     gray = cv2.cvtColor(frame_small, cv2.COLOR_BGR2GRAY)
 
@@ -26,11 +25,9 @@ def process_frame(frame):
     if len(faces) == 0:
         return frame_small
 
-    # Largest face
     (x, y, w, h) = max(faces, key=lambda f: f[2]*f[3])
 
-    # 🔥 Run DeepFace only every 5 frames (IMPORTANT for speed)
-    if frame_count % 5 == 0:
+    if frame_count % 4 == 0:
         face_img = frame_small[y:y+h, x:x+w]
 
         try:
@@ -41,16 +38,26 @@ def process_frame(frame):
             )
 
             emotion_data = result[0]['emotion']
+            emotion_data['angry'] *= 1.2
+            emotion_data['surprise'] *= 1.15
+            emotion_history.append(emotion_data)
 
-            # Get dominant emotion
-            last_emotion = max(emotion_data, key=emotion_data.get)
-            last_confidence = emotion_data[last_emotion]
+            if len(emotion_history) > 5:
+                emotion_history.pop(0)
 
         except:
-            pass  # ignore errors safely
+            pass
 
-    # 🔥 Draw result
-    if last_confidence > 30:
+    if len(emotion_history) == 0:
+        return frame_small
+    avg_emotions = {}
+
+    for key in emotion_history[0].keys():
+        avg_emotions[key] = np.mean([e[key] for e in emotion_history])
+
+    last_emotion = max(avg_emotions, key=avg_emotions.get)
+    last_confidence = avg_emotions[last_emotion]
+    if last_confidence > 25:
         label = f"{last_emotion.capitalize()} ({last_confidence:.1f}%)"
 
         cv2.rectangle(frame_small, (x, y), (x+w, y+h), (0,255,0), 2)
